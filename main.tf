105f7350a57021c76ec64ce3eea67196e82b55f1
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-13 22:46:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,6 @@ terraform {
     storage_account_name = "akmal3m4"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    use_msi              = true
-    subscription_id      = "f51924a0-0777-4c94-8948-867871c84397"
-    tenant_id            = "00000000-0000-0000-0000-000000000000"
   }
 }
 
