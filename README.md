7f45dedbdec76ba04466e7d3b5c140ce928de333
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-14 20:13:54 +0800

    update

--- README.md
+++ README.md
@@ -11,4 +11,18 @@ az ad sp create-for-rbac --name osba -o table
 useful azure cli commands
 ```
 az account list
+az login --service-principal -u xxx -p xxx--tenant xxx --allow-no-subsciptions
+az login --service-principal -u xxx -p xxx --tenant xxx
+
+sudo AZURE_CLIENT_ID=xxxAZURE_CLIENT_SECRET=xxx AZURE_TENANT_ID=xxx KEY_VAULT_NAME=kv14012022 node app.js
 ```
+
+
+Reference
+https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal
+
+
+use sharedaccess for normal api
+
+create bearer token if need to use management api
+https://social.technet.microsoft.com/wiki/contents/articles/53488.azure-rest-api-how-to-create-bearer-token.aspx
--- app.js
+++ app.js
@@ -5,36 +5,33 @@ const bodyParser = require("body-parser");
 const axios = require('axios');
 const router = express.Router();
 const app = express();
-const { SecretClient } = require("@azure/keyvault-secrets");
-const { DefaultAzureCredential } = require("@azure/identity");
+const {
+    SecretClient
+} = require("@azure/keyvault-secrets");
+const {
+    DefaultAzureCredential
+} = require("@azure/identity");
 const dotenv = require("dotenv");
 dotenv.config();
-
+var secret;
 async function main() {
 
-    // DefaultAzureCredential expects the following three environment variables:
-    // - AZURE_TENANT_ID: The tenant ID in Azure Active Directory
-    // - AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
-    // - AZURE_CLIENT_SECRET: The client secret for the registered application
     const credential = new DefaultAzureCredential();
-  
+
     const keyVaultName = process.env.KEY_VAULT_NAME;
     const url = "https://" + keyVaultName + ".vault.azure.net";
-    console.log(url);
     const client = new SecretClient(url, credential);
-  
+
 
     // Read the secret we created
-    const secret = await client.getSecret("api-token").catch(function(err) {
+    secret = await client.getSecret("api-key").catch(function(err) {
 
         console.log(err);
         return;
     });;
+}
 
-    console.log("secret: ", secret);
-  }
-
-  main();
+main();
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -47,21 +44,22 @@ router.post('/', (request, response) => {
 
 router.get('/create', async (request, response) => {
 
-var user=request.query.name;
-axios
-    .post('https://management.azure.com/subscriptions/f51924a0-0777-4c94-8948-867871c84397/resourcegroups/ak?api-version=2021-04-01', {
-
-
-    })
-    .then(res => {
+    var user = request.query.name;
+    axios
+        .post('https://management.azure.com/subscriptions/f51924a0-0777-4c94-8948-867871c84397/resourcegroups/ak?api-version=2021-04-01', {
+            headers: {
+                Authorization: 'Bearer ' + secret
+            }
+        })
+        .then(res => {
             response.status(200);
             response.send(res.data["Resource group created"]);
-    })
-    .catch(error => {
-        console.error(error)
-        response.statusCode = 401;
-        response.send(error);
-    })
+        })
+        .catch(error => {
+            console.error(error)
+            response.statusCode = 401;
+            response.send(error);
+        })
 
 
 
