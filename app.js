6fbce9bce5e293e264647d8489472ece2cb5cff7
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-14 21:20:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ router.get('/create', async (request, response) => {
               }
 
             await axios
-            .put('https://management.azure.com/subscriptions/b7c92367-e09f-49dd-b4d7-f9889803f853/resourcegroups/'+user+'?api-version=2021-04-01', config,{
+            .post('https://management.azure.com/subscriptions/b7c92367-e09f-49dd-b4d7-f9889803f853/resourcegroups/'+user+'?api-version=2021-04-01', config,{
                 location: "Switzerland North"
             })
             .then(res1 => {
