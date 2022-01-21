5d76a009c349435d058bd4612510d7a347c9ca9d
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-22 02:24:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ router.post('/create', async (request, response) => {
 
                             await axios.post('https://management.azure.com/subscriptions/b7c92367-e09f-49dd-b4d7-f9889803f853/resourcegroups/' + user + '/providers/Microsoft.OperationalInsights/workspaces/' + orderid + '/sharedKeys?api-version=2020-08-01',
                             config)
-                            .then(async res3 => {
+                            .then(res3 => {
                                 // send primary key to email
                                 //console.log(res2.data["properties"]["customerId"])
                                 
